d878c3edc5b488af13c4f4ddac93466a1cdd8cda
Катя <@DESKTOP-VT3CEPG>
2023-07-05 18:51:06 +0300

    0

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,10 +1,49 @@
-﻿namespace C_
+﻿using System.Globalization;
+using System.Text;
+
+namespace C_
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            CultureInfo culture = new CultureInfo("uk-UA");
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CultureInfo.DefaultThreadCurrentUICulture = culture;
+            Console.OutputEncoding = Encoding.UTF8;
+
+
+            int switch_on;
+
+            do
+            {
+                Console.WriteLine("\n#===========================#");
+                Console.WriteLine(" |       1 -  case 1       |");
+                Console.WriteLine(" |       2 -  case 2       |");
+                Console.WriteLine(" |       3 -  case 3       |");
+                Console.WriteLine(" |       4 -  case 4       |");
+                Console.WriteLine(" |       0 -   exit        |");
+                Console.WriteLine("#===========================#");
+                Console.Write("Виберіть пункт меню -> ");
+                switch_on = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\n");
+                Console.Clear();
+
+                switch (switch_on)
+                {
+                    case 1:
+                        {
+
+                        }break;
+                    case 2:
+                        {
+
+                        }break;
+                    default:
+                        break;
+                }
+
+            }while (switch_on != 0);
+                }
     }
 }
